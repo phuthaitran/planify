457f35c8b3337ced52116f80acd3bbae95bd50da
VPDT <vudiepthao@gmail.com>
2025-12-08 19:33:26 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+/*
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginSignup from './LoginSignup'
+*/
+import LoginSignup from './pages/LoginSignup'
 
 function App() {
   const [count, setCount] = useState(0)
