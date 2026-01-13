4c75db73bdb40bd7fea07adb02e05d9aabb8423e
Phan Sy Long <phansylong12mk@gmail.com>
2026-01-13 13:43:54 +0700

    create realtime gateway to frontend

--- backend/src/main/java/com/planify/backend/service/DealineReminderService.java
+++ backend/src/main/java/com/planify/backend/service/DealineReminderService.java
@@ -5,28 +5,39 @@ import com.planify.backend.repository.PlanRepository;
 import com.planify.backend.service.NotificationProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
+@Service
 @RequiredArgsConstructor
-public class DealineScheduler {
+public class DealineService{
 
-        private final PlanRepository planRepository;
+    private final PlanRepository planRepository;
+    private final NotificationProducer notificationProducer;
 
-        private final NotificationProducer notificationProducer;
+    @Scheduled(fixedRate = 60000)
+    @Transactional
+    public void scan() {
 
-        @Scheduled(fixedRate = 60000)
-        @Transactional
-        public void scan() {
+        // Tạo Authentication giả lập với quyền ADMIN
+        var auth = new UsernamePasswordAuthenticationToken(
+                "system",
+                null,
+                List.of(new SimpleGrantedAuthority("SCOPE_ADMIN"))
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
+        try {
             LocalDateTime now = LocalDateTime.now();
+
             // reminder
             List<Plan> reminders = planRepository.findRemindersPlanWithDetails(now);
-
             for (Plan plan : reminders) {
                 notificationProducer.sendReminder(plan);
                 plan.setReminderSent(true);
@@ -34,14 +45,18 @@ public class DealineScheduler {
 
             // expired
             List<Plan> duePlans = planRepository.findDuePlansWithDetails(now);
-
             for (Plan plan : duePlans) {
-                notificationProducer.sendDue(plan);
+                notificationProducer.sendDue(plan);       // @PreAuthorize trong method này sẽ pass
                 plan.setExpiredSent(true);
             }
 
             planRepository.saveAll(reminders);
             planRepository.saveAll(duePlans);
+
+        } finally {
+            SecurityContextHolder.clearContext();  // Xóa Authentication sau khi task xong
         }
     }
+}
+
 
